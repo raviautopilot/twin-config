eb6ed20b24c7039e64a77c5aea355c23c8a3fdea
raviautopilot <ravi.autopilot@gmail.com>
2026-06-08 18:29:52 +0530

    added query.sql (#13)
    Co-authored-by: Ravi Jaganathan <raviregi@gmail.com>

---
+++ user-documentation/query.sql
@@ -0,0 +1,16 @@
+select * from cfg_modules;
+
+
+select * from cfg_type;
+
+select * from cfg_event_types;
+
+select * from cfg_dimensions;
+
+select * from cfg_attribute_keys;
+
+select * from event_details;
+
+select * from twin_event;
+
+select * from twin_impact;
